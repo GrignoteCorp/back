517b728562cbdf7308e673fa9d4a5d7a22730fb7
Lucie Ménard <lulu.menard@gmail.com>
2024-08-04 19:33:56 +0200

    feat: init projet

---
+++ src/test/java/fr/grignotte/grignotte_back/GrignotteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.grignotte.grignotte_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrignotteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
